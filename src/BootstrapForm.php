bbfd0e67476788e1826619bbbca6a39389ff1d91
Michael Burgin <realripley00@gmail.com>
2018-02-12 14:52:09 -0500

    updated form field classes.

--- src/BootstrapForm.php
+++ src/BootstrapForm.php
@@ -813,7 +813,7 @@ class BootstrapForm
      */
     protected function getFormGroupOptions($name = null, array $options = [])
     {
-        $class = 'form-group';
+        $class = 'form-group row';
 
         if ($name) {
             $class .= ' ' . $this->getFieldErrorClass($name);
@@ -863,7 +863,7 @@ class BootstrapForm
      */
     protected function getLabelOptions(array $options = [])
     {
-        $class = 'control-label';
+        $class = 'form-control-label';
         if ($this->isHorizontal()) {
             $class .= ' ' . $this->getLeftColumnClass();
         }
